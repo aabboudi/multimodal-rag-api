02f6a069beabdcbd3fb8287450801ca8d85743a7
aabboudi <abboudi.abdellah@gmail.com>
2024-09-25 18:25:37 +0100

    Refactored loaders to use dispatcher function for dynamic file type handling

--- app/api/v1/api_v1.py
+++ app/api/v1/api_v1.py
@@ -2,15 +2,12 @@ from fastapi import APIRouter, UploadFile, File
 from datetime import datetime, timezone
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 
-from app.rag.load import load_docs
+from app.rag.load import load_file
 from app.rag.embed import embed_docs
 from app.rag.query import query_rag
 from app.services.utils import sanitize_string
 from app.services.validators import *
 from app.config import EMBEDDING_MODEL
-
-# embeddings = OllamaEmbeddings(model="nomic-embed-text")
-embeddings = OllamaEmbeddings(model=EMBEDDING_MODEL)
 
 router = APIRouter()
 
@@ -22,29 +19,30 @@ def health_check():
 # TODO: Add filter to store files with the same format in a single directory
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-  format = validate_file_ext(file.filename)
+  file_format = validate_file_ext(file.filename)
   validate_file_size(file)
 
   now = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%S")
   clean_filename = sanitize_string(file.filename)
-  file_location = f"data/{format}/{now}_{clean_filename}"
+  file_location = f"data/{file_format}/{now}_{clean_filename}"
 
   with open(file_location, "wb+") as file_object:
     file_object.write(file.file.read())
 
-  docs = load_docs(format, f"data/{format}")
-  embed_docs(docs)
+  file = load_file(file_location, file_format)
+  embed_docs(file)
 
   return {
-    "file_name": clean_filename,
-    "file_format": format,
-    "embedding": "started"
+    "filename": clean_filename,
+    "file_format": file_format,
+    "embedding_status": "started",
   }
 
 
 @router.post("/ask")
 def ask_embedded_files(query: str):
   query = query
+  embeddings = OllamaEmbeddings(model=EMBEDDING_MODEL)
   response = query_rag(query, "chroma", embeddings)
   return {
     "query": query,
